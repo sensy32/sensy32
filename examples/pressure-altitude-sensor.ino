ba8c3dd6192b780f1e5826cf397741aa11fba2f7
Sensy32 <support@sensy32.io>
2024-12-23 17:32:11 +0100

    Update pressure-altitude-sensor.ino

--- examples/pressure-altitude-sensor.ino
+++ examples/pressure-altitude-sensor.ino
@@ -3,63 +3,15 @@
   This code reads temperature, pressure, and altitude data from the BMP388 sensor, displays it on an OLED screen, and sends it to a server via WiFi to (https://sensy32.io)
 */
 
+#include "config.h" // config file shared between all files
+
 #include <BMP388_DEV.h>
-#include <WiFi.h>
-#include <WiFiClientSecure.h> 
-#include <HTTPClient.h>
-#include <ss_oled.h>
-
-//Board configuration
-String apiKey = "board-api-key"; // Change here your Board API key 
-
-// Web Server Configuration
-const char *ssid = "wifi-name"; // Change here your Wi-Fi network SSID (name) 
-const char* password = "wifi-password"; // Change here your Wi-Fi network password 
-const char *server = "https://sensy32.io";
-const int port = 443;
 
 //Sensor Configuration
 volatile boolean dataReady = false;
 float temperature, pressure, altitude;
 int int_pin = 33;
 BMP388_DEV bmp388;  // Instantiate (create) a BMP388_DEV object and set-up for I2C operation (address 0x77) 
-
-//OLED / LCD Configuration
-
-// if your system doesn't have enough RAM for a back buffer, comment out
-// this line (e.g. ATtiny85)
-#define USE_BACKBUFFER
-
-#ifdef USE_BACKBUFFER
-static uint8_t ucBackBuffer[1024];
-#else
-static uint8_t *ucBackBuffer = NULL;
-#endif
-
-// Use -1 for the Wire library default pins
-// or specify the pin numbers to use with the Wire library or bit banging on any GPIO pins
-// These are the pin numbers for the M5Stack Atom default I2C
-#define SDA_PIN -1
-#define SCL_PIN -1
-// Set this to -1 to disable or the GPIO pin number connected to the reset
-// line of your display if it requires an external reset
-#define RESET_PIN -1
-// let ss_oled figure out the display address
-#define OLED_ADDR -1
-// don't rotate the display
-#define FLIP180 0
-// don't invert the display
-#define INVERT 0
-// Bit-Bang the I2C bus
-#define USE_HW_I2C 1
-
-// Change these if you're using a different OLED display
-#define MY_OLED OLED_128x64
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 64
-//#define MY_OLED OLED_64x32
-//#define OLED_WIDTH 64
-//#define OLED_HEIGHT 32
 
 SSOLED ssoled;
 
